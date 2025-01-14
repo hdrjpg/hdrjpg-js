a183f489383800f15309744dae3868a0c1b17e16
Loren <loren@tin.cat>
2025-01-14 08:48:04 +0100

    Add Microsoft Edge compatibility

--- src/hdrjpg.js
+++ src/hdrjpg.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
         /^((?!chrome|android).)*safari/i.test(navigator.userAgent) /* Target Safari */
     ) {
         document.querySelectorAll('.hdrjpg[data-mp4-src]').forEach((element) => {
-            element.querySelector('img').remove();
+            element.querySelector('picture').remove();
             let video = document.createElement('video');
             video.src = element.dataset.mp4Src;
             video.muted = true;
